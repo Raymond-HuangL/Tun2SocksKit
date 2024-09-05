bb189d6148f49d8ceca3581a667c53dc603bdb39
Raymond <raymond0huang@gmail.com>
2024-09-05 16:37:01 +0000

    Tun2SocksKit Release 2.7.4!!!

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     ),
     .binaryTarget(
         name: "HevSocks5Tunnel",
-        url: "https://github.com/arror/Tun2SocksKit/releases/download/2.5.3/HevSocks5Tunnel.xcframework.zip",
-        checksum: "ea701a4eb670e9bb5cc10d7e2f9238c8253e6edc7be03bc699d0d675a1d7154c"
+        url: "https://github.com/Raymond-HuangL/Tun2SocksKit/releases/download/2.7.4/HevSocks5Tunnel.xcframework.zip",
+        checksum: "6eba76e83b623617f3a3a01176659b6c85e168effe0e0e2d3a870dc4885450a7"
     )
   ]
 )
